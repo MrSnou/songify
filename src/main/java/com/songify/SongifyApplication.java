01497c72997250a50a45c3743aac3b7c0bbd4d7b
Jakub <jakubfrolow@gmail.com>
2025-12-03 10:07:12 +0100

    Initial commit

--- src/main/java/com/songify/SongifyApplication.java
+++ src/main/java/com/songify/SongifyApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SongifyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SongifyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SongifyApplication.class, args);
+    }
+
+
 
 }
