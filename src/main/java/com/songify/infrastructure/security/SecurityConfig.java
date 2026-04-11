c0cb00b55983b7ce70eeabad03b54b1170db8a92
Jakub <jakubfrolow@gmail.com>
2026-04-11 19:01:04 +0200

    Implementation of Spring security.
    - Added cors congifuration to SecurityConfig.java.

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -6,10 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 class SecurityConfig {
@@ -46,6 +51,7 @@ class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(c -> c.disable());
+        http.cors(corsConfigurerCustomizer());
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         http.authorizeHttpRequests(
@@ -78,5 +84,20 @@ class SecurityConfig {
                                 .requestMatchers(HttpMethod.DELETE, "/genres/**" ).hasRole("ADMIN")
                                 .anyRequest().authenticated());
         return http.build();
+    }
+
+    public Customizer<CorsConfigurer<HttpSecurity>> corsConfigurerCustomizer() {
+        return c -> {
+            CorsConfigurationSource source = request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(
+                        List.of("http://localhost:3000"));
+                config.setAllowedMethods(
+                        List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+                config.setAllowedHeaders(List.of("*"));
+                return config;
+            };
+            c.configurationSource(source);
+        };
     }
 }
